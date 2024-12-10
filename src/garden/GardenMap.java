f709b00b7adbd4c9d7140a5ad9a72d9f36cf997c
can-ylmaz <canyilmaz.5010@gmail.com>
2024-12-10 21:35:24 +0300

    implementation in progress

--- src/garden/GardenMap.java
+++ src/garden/GardenMap.java
@@ -1,17 +1,42 @@
 package garden;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GardenMap {
     public static final int ROW_COUNT = 6;
     public static final int COLUMN_COUNT = 8;
     public static final int STATUES = 7;
 
-    private ArrayList<ArrayList<GardenSquare>> map;
+    private List<List<GardenSquare>> map;
 
-    /* constructor will initialize empty map, randomly place 7 statues and determine the goal position, may take randomizer as input */
+    public GardenMap() {
+        initializeMap();
+        initializeStatues();
+    }
+
+    public boolean putItem(Placable item, GardenPosition position) {
+
+    }
 
     public GardenSquare getSquare(GardenPosition position) {
         return map.get(position.getRow()).get(position.getColumn());
+    }
+
+    private void initializeMap() {
+        map = new ArrayList<>();
+        /*
+         * i and j are dummy counters, never used
+         */
+        for (int i = 0; i < ROW_COUNT; i++) {
+            List<GardenSquare> newRow = new ArrayList<>();
+            for (int j = 0; j < COLUMN_COUNT; j++) {
+                newRow.add(new GardenSquare());
+            }
+            map.add(newRow);
+        } 
+    }
+    private void initializeStatues() {
+
     }
 }
