ecd54a4de2cfecaf300747c0f2d99ed1b650d098
can-ylmaz <canyilmaz.5010@gmail.com>
2024-12-10 21:37:41 +0300

    added constructor

--- src/gardenobjects/Statue.java
+++ src/gardenobjects/Statue.java
@@ -2,6 +2,10 @@ package gardenobjects;
 
 public class Statue extends GardenObject{
 
+    public Statue(String ID) {
+        setID(ID);
+    }
+
     @Override
     public boolean checkByType(String type) {
         return false;
