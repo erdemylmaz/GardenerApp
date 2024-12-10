4d9970c4588f76decf31fc5038a5606e14de5668
can-ylmaz <canyilmaz.5010@gmail.com>
2024-12-10 21:40:53 +0300

    implemented

--- src/randomizers/GardenerRandomizer.java
+++ src/randomizers/GardenerRandomizer.java
@@ -1,16 +1,39 @@
 package randomizers;
 
-import java.util.Random;
-
+import garden.GardenMap;
 import garden.GardenPosition;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import pollen.LightColor;
 import pollen.PollenCloud;
+import pollen.PollenTypes;
 
 public class GardenerRandomizer {
-    private Random randomizer;
+    private final Random randomizer;
+
+    public GardenerRandomizer() {
+        randomizer = new Random();
+    }
+
     public GardenPosition randomPosition() {
-        return null;
+        return new GardenPosition(randomizer.nextInt(GardenMap.ROW_COUNT), randomizer.nextInt(GardenMap.COLUMN_COUNT));
     }
     public PollenCloud randomCloud() {
-        return null;
+        Set<PollenTypes> types = new HashSet<>();
+        if (randomizer.nextBoolean()) types.add(PollenTypes.BUSH);
+        if (randomizer.nextBoolean()) types.add(PollenTypes.TREE);
+        if (randomizer.nextBoolean()) types.add(PollenTypes.FLOWER);
+
+        if (types.isEmpty()) { // ensure there is at least one pollen type in the cloud
+            types.add(PollenTypes.values()[randomizer.nextInt(3)]);
+        }
+
+        Set<LightColor> colors = new HashSet<>();
+        if (randomizer.nextBoolean()) colors.add(LightColor.RED);
+        if (randomizer.nextBoolean()) colors.add(LightColor.BLUE);
+        if (randomizer.nextBoolean()) colors.add(LightColor.GREEN);
+
+        return new PollenCloud(types, colors);
     }
 }
